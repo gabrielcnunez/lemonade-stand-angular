a0499f9853d7fc4952461fdd649cae00d9db5a04
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-09-14 15:45:14 -0700

    Create lemonade product component

--- src/app/lemonade/lemonade.component.ts
+++ src/app/lemonade/lemonade.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductComponent } from "./product/product.component";
 
 @Component({
   selector: 'app-lemonade',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './lemonade.component.html',
   styleUrl: './lemonade.component.css'
 })
---
+++ src/app/lemonade/product/product.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  @Input() name: string = ""
+}
