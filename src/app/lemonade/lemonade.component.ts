6c8cdfc9eea054aea61be104bc3483ff1a7b6858
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-09-14 14:13:08 -0700

    Initial commit

---
+++ src/app/lemonade/lemonade.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lemonade',
+  standalone: true,
+  imports: [],
+  templateUrl: './lemonade.component.html',
+  styleUrl: './lemonade.component.css'
+})
+export class LemonadeComponent {
+
+}
